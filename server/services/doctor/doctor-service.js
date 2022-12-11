18385d675aafced4687fe5635f1115e8da90450b
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2022-12-11 12:13:46 +0530

    complete doctor routes

--- server/services/doctor/doctor-service.js
+++ server/services/doctor/doctor-service.js
@@ -1,10 +1,26 @@
 const Doctor = require("../../utils/doctor/Doctor")
 
-const getAllDoctors = async () => {};
+const getAllDoctors = async () => {
+	const allDoctors = await Doctor.getAllDoctors()
+	return allDoctors
+};
 
-const addNewDoctor = async () => {};
+const addNewDoctor = async (newDoctor) => {
+	const dateRegistered = new Date()
 
-const getDoctorById = async () => {};
+	const doctorToInsert = {
+		...newDoctor,
+    dateRegistered
+	}
+
+	const added = await Doctor.addNewDoctor(doctorToInsert)
+	return added
+};
+
+const getDoctorById = async (id) => {
+	const doctor = await Doctor.getDoctorById(id)
+  return doctor
+};
 
 module.exports = {
 	getAllDoctors,
--- server/utils/doctor/Doctor.js
+++ server/utils/doctor/Doctor.js
@@ -1,11 +1,83 @@
 const mongoose = require("mongoose");
-const model__doctor = require("../../models/doctor/doctor-model")
+const model__doctor = require("../../models/doctor/doctor-model");
 
-const getAllDoctors = async () => {};
+const getAllDoctors = async () => {
+	return await model__doctor.find(
+		{},
+		{
+			id: 1,
+			name: 1,
+			phoneNo: 1,
+			email: 1,
+			address: 1,
+			qualifications: 1,
+			dateRegistered: 1,
+		}
+	);
+};
 
-const addNewDoctor = async () => {};
+const addNewDoctor = async (add) => {
+	let doctor = new model__doctor({
+		id: add.id,
+		name: add.name,
+		phoneNo: add.phoneNo,
+		email: add.email,
+		address: add.address,
+		qualifications: add.qualifications,
+		dateRegistered: add.dateRegistered,
+		password: add.password,
+	});
 
-const getDoctorById = async () => {};
+	doctor.setPassword(add.password);
+
+	const emailCheck = await model__doctor
+		.find({ email: add.email })
+		.exec();
+	if (emailCheck.length > 0) {
+		return "email available";
+	}
+
+	const idCheck = await model__doctor
+		.find({ id: add.id })
+		.exec();
+	if (idCheck.length > 0) {
+		return "doctor id available";
+	}
+
+	try {
+		await doctor.save();
+	} catch (e) {
+		console.log(e);
+		return "db error";
+	}
+
+	const addedOne = {
+		id: doctor.id,
+    name: doctor.name,
+		phoneNo: doctor.phoneNo,
+		email: doctor.email,
+    address: doctor.address,
+		qualifications: doctor.qualifications,
+		dateRegistered: doctor.dateRegistered,
+	}
+	
+	return addedOne
+};
+
+const getDoctorById = async (id) => {
+	return await model__doctor.find(
+		{ id: id },
+		{
+			id: 1,
+			name: 1,
+			phoneNo: 1,
+			email: 1,
+			address: 1,
+			qualifications: 1,
+			dateRegistered: 1,
+		}
+	);
+};
 
 module.exports = {
 	getAllDoctors,
